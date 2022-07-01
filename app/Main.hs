4d51557d5363331aa40e6acd2e7bdc3ad201acaa
GalileuGalilei <alfredocossetin@outlook.com>
2022-06-30 23:05:05 -0300

    pong pronto

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,22 @@
 module Main(main) where
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.Pure.Game
+
+type Radius = Float 
+type Position = (Float, Float)
+
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
 
 window :: Display
-window = InWindow "Nice Window" (800, 800) (10, 10)
+window = InWindow "Pong" (width, height) (offset, offset)
 
 background :: Color
-background = white
+background = black
 
 data PongGame = Game
   { ballLoc :: (Float, Float)  -- ^ Pong ball (x, y) location.
@@ -57,5 +67,68 @@ render game =
     paddleColor = light (light blue)
 
 
+moveBall :: Float -> PongGame -> PongGame
+moveBall seconds game = game { ballLoc = (x', y') }
+  where
+    -- Old locations and velocities.
+    (x, y) = ballLoc game
+    (vx, vy) = ballVel game
+
+    -- New locations.
+    x' = x + vx * seconds
+    y' = y + vy * seconds
+
+fps :: Int
+fps = 60
+
+-- | Given position and radius of the ball, return whether a collision occurred.
+wallCollision :: Position -> Radius -> Bool 
+wallCollision (_, y) radius = topCollision || bottomCollision
+  where
+    topCollision    = y - radius <= -fromIntegral width / 2 
+    bottomCollision = y + radius >=  fromIntegral width / 2
+
+wallBounce :: PongGame -> PongGame
+wallBounce game = game { ballVel = (vx, vy') }
+  where
+    -- Radius. Use the same thing as in `render`.
+    radius = 10
+
+    -- The old velocities.
+    (vx, vy) = ballVel game
+
+    vy' = if wallCollision (ballLoc game) radius
+          then
+             -- Update the velocity.
+             -vy
+           else
+            -- Do nothing. Return the old velocity.
+            vy
+
+-- | Respond to key events.
+handleKeys :: Event -> PongGame -> PongGame
+
+-- For an 's' keypress, reset the ball to the center.
+handleKeys (EventKey (Char 's') down _ _) game =
+  game {player1 = py + speed}  
+  where
+    speed = if down==Up then 5
+      else -5
+    py = player1 game
+
+handleKeys (EventKey (Char 'w') down _ _) game =
+  game { player1 = 
+    if down==Up then py
+      else py + 1}
+  where
+    py = player1 game
+-- Do nothing for all other events.
+handleKeys _ game = game
+
+
+-- | Update the game by moving the ball and bouncing off walls.
+update :: Float -> PongGame -> PongGame
+update seconds = wallBounce . moveBall (seconds*5)
+
 main :: IO ()
-main = display window background (render initialState)
+main = play window background fps initialState render handleKeys update
--- dist-newstyle/build/x86_64-windows/ghc-8.10.7/SnakeGame-0.1.0.0/x/SnakeGame/build/SnakeGame/autogen/cabal_macros.h
+++ dist-newstyle/build/x86_64-windows/ghc-8.10.7/SnakeGame-0.1.0.0/x/SnakeGame/build/SnakeGame/autogen/cabal_macros.h
@@ -111,16 +111,6 @@
   (major1) == 8 && (major2) <  10 || \
   (major1) == 8 && (major2) == 10 && (minor) <= 7)
 #endif /* MIN_TOOL_VERSION_runghc */
-/* tool strip-2.30 */
-#ifndef TOOL_VERSION_strip
-#define TOOL_VERSION_strip "2.30"
-#endif /* TOOL_VERSION_strip */
-#ifndef MIN_TOOL_VERSION_strip
-#define MIN_TOOL_VERSION_strip(major1,major2,minor) (\
-  (major1) <  2 || \
-  (major1) == 2 && (major2) <  30 || \
-  (major1) == 2 && (major2) == 30 && (minor) <= 0)
-#endif /* MIN_TOOL_VERSION_strip */
 
 #ifndef CURRENT_COMPONENT_ID
 #define CURRENT_COMPONENT_ID "SnakeGame-0.1.0.0-inplace-SnakeGame"
