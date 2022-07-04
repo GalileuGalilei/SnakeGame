262487d4e2d86f87dad621284ae08ef10af4fe68
GalileuGalilei <alfredocossetin@outlook.com>
2022-07-03 21:42:16 -0300

    tudo funcionando

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,12 @@ import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.Pure.Game
 import System.Random
-import GHC.IO.Encoding (TextEncoding(mkTextEncoder))
-import Control.Arrow (Arrow(first, second))
 
 width, height, cols, rows, offset :: Int
 width = 480
 height = 480
-cols = 32
-rows = 32
+cols = 20
+rows = 20
 offset = 10 
 
 window :: Display
@@ -25,16 +23,18 @@ data GameState = State
     snakeDir :: Point,
     foodLoc :: Point,
     isGameover :: Bool,
-    foodSeed :: StdGen
+    foodSeed :: StdGen,
+    snakeColor :: Color 
   }
 
 initialState :: GameState
 initialState = State
-  { snake = [(fX, fY), (fX + 1, fY),(fX + 2, fY), (fX + 3, fY)],
+  { snake = [(fX, fY), (fX - 1, fY),(fX - 2, fY), (fX - 3, fY)],
     snakeDir = (1,0),
-    foodLoc = (20,20), 
+    foodLoc = (fX - 4, fY - 4), 
     isGameover = False,
-    foodSeed = mkStdGen 100
+    foodSeed = mkStdGen 80,
+    snakeColor = white
   }
   where 
     fX = fromIntegral rows / 2
@@ -46,7 +46,8 @@ render :: GameState -> Picture
 render game = pictures (food : (snakeBody ++ walls)) 
   where
     food = mkNode (dark red) (foodLoc game)
-    snakeBody = map (mkNode white) (snake game)
+    snakeBody = map (mkNode newColor) (snake game)
+    newColor = snakeColor game
 
     -- paredes do topo e dos lados
     walls :: [Picture]
@@ -75,30 +76,61 @@ render game = pictures (food : (snakeBody ++ walls))
         rectX = fromIntegral (width + offset) / (fromIntegral cols)
         rectY = fromIntegral (height + offset) / (fromIntegral rows)
 
-moveSnake :: ViewPort -> Float -> GameState -> GameState
-moveSnake _ _ game = game {snake = [(x + xDir, y + yDir) | (x,y) <- snakeBody], foodLoc = food, foodSeed = newSeed}
+update :: Float -> GameState -> GameState
+update _ game = if checkGameOver snakeBody then game { snakeColor = red }
+  else game {
+  snake = newHead : tail, foodLoc = newFood, foodSeed = newSeed
+  }
   where
-    snakeBody = snake game 
+    tail = if wasFoodEaten then snakeBody else removePoint snakeBody $ last snakeBody
+    (newFood, newSeed) = if wasFoodEaten then generateNewFood snakeBody curSeed else (curFood, curSeed)
+    curFood = foodLoc game
+    curSeed = foodSeed game
+    wasFoodEaten = newHead == curFood
+    newHead = (x + xDir, y + yDir)
     xDir = fst dir
     yDir = snd dir
     dir = snakeDir game
-    (food, newSeed) = generateNewFood snakeBody (foodSeed game)
+    (x,y) = head snakeBody
+    snakeBody = snake game 
+
+removePoint :: [Point] -> Point -> [Point]
+removePoint list elem = filter (/= elem) list
 
 generateNewFood :: [Point] -> StdGen -> (Point, StdGen)
-generateNewFood snake stdGen =  if (fromIntegral foodX, fromIntegral foodY) `elem` snake
-                                then generateNewFood snake stdGen3
-                                else ((fromIntegral foodX, fromIntegral foodY), stdGen3)
-        where   (foodX, stdGen2) = randomR (1, cols) stdGen
-                (foodY, stdGen3) = randomR (1, rows) stdGen2
+generateNewFood snake stdGen =  
+  if (fromIntegral foodX, fromIntegral foodY) `elem` snake
+    then generateNewFood snake stdGen3
+    else ((fromIntegral foodX, fromIntegral foodY), stdGen3)
+        where 
+          (foodX, stdGen2) = randomR (1, cols - 1) stdGen
+          (foodY, stdGen3) = randomR (1, rows - 1) stdGen2
+
+checkGameOver :: [Point] -> Bool
+checkGameOver snake =   headX == 0 || headX == fromIntegral cols || 
+                        headY == 0 || headY == fromIntegral rows ||
+                        head' `elem` tail'
+    where   head' = head snake
+            (headX, headY) = head'
+            tail' = tail snake
+
+handleKeys :: Event -> GameState -> GameState
+handleKeys (EventKey (Char 'd') _ _ _) game =
+  game { snakeDir = if curDir /= (-1, 0) then (1,0) else curDir }
+    where curDir = snakeDir game
+handleKeys (EventKey (Char 'a') _ _ _) game =
+  game { snakeDir = if curDir /= (1, 0) then (-1,0) else curDir }
+    where curDir = snakeDir game
+handleKeys (EventKey (Char 'w') _ _ _) game =
+  game { snakeDir = if curDir /= (0, -1) then (0,1) else curDir }
+    where curDir = snakeDir game
+handleKeys (EventKey (Char 's') _ _ _) game =
+  game { snakeDir = if curDir /= (0, 1) then (0,-1) else curDir }
+    where curDir = snakeDir game
+handleKeys _ game = game
 
 fps :: Int
-fps = 60
-
-
-
-
-
-
+fps = 8
 
 main :: IO ()
-main = simulate window background 2 initialState render moveSnake
+main = play window background fps initialState render handleKeys update
